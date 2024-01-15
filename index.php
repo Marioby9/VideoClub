5aedf12a75f4c621431a995966f72db43af31fde
Mario Martín <mmartin.mrmg@gmail.com>
2024-01-15 12:34:32 +0100

    refactor: 🔨 Fix MVC

--- index.php
+++ index.php
@@ -15,70 +15,50 @@
     }
     $ORM = new ORM();
     
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="<?php echo $ruta . "/public/css/ViewsCSS/index.css"; ?>">
-    <link rel="icon" href="./public/img/logos/whiteLogoIco.ico" type="image/x-icon">
-    <title>VideoClub</title>
-</head>
-<body class="center">
-    <section class="page">
-        <?php include_once "./src/Components/Menu.php"; ?>
-        <main id="main">
-            <?php 
-                if(isset($_GET["ruta"])){
-                    if($_GET["ruta"] == "home"){
-                        include_once "./src/View/HomeView.php";
-                    }
-                    else if($_GET["ruta"] == "movies"){
-                        include_once "./src/Controller/AllProducts/MoviesController.php";
-                    }
-                    else if($_GET["ruta"] == "books"){
-                        include_once "./src/Controller/AllProducts/BooksController.php";
-                    }
-                    else if($_GET["ruta"] == "music"){
-                        include_once "./src/Controller/AllProducts/MusicController.php";
-                    }
-                    else if($_GET["ruta"] == "addMovie"){
-                        include_once "./src/Controller/AddProduct/AddMovieController.php";
-                    }
-                    else if($_GET["ruta"] == "addBook"){
-                        include_once "./src/Controller/AddProduct/AddBookController.php";
-                    }
-                    else if($_GET["ruta"] == "addDisc"){
-                        include_once "./src/Controller/AddProduct/AddDiscController.php";
-                    }
-                    else if($_GET["ruta"] == "singleMovie" && isset($_GET["id"])){
-                        include_once "./src/Controller/ShowProduct/ShowMovieController.php";
-                    }
-                    else if($_GET["ruta"] == "singleBook" && isset($_GET["id"])){
-                        include_once "./src/Controller/ShowProduct/ShowBookController.php";
-                    }
-                    else if($_GET["ruta"] == "singleDisc" && isset($_GET["id"])){
-                        include_once "./src/Controller/ShowProduct/ShowDiscController.php";
-                    }
-                    else if($_GET["ruta"] == "updateMovie" && isset($_GET["id"])){
-                        include_once "./src/Controller/UpdateProduct/UpdateMovieController.php";
-                    }
-                    else if($_GET["ruta"] == "updateBook" && isset($_GET["id"])){
-                        include_once "./src/Controller/UpdateProduct/UpdateBookController.php";
-                    }
-                    else if($_GET["ruta"] == "updateDisc" && isset($_GET["id"])){
-                        include_once "./src/Controller/UpdateProduct/UpdateDiscController.php";
-                    }
-                }
-                else{
+    include_once "./src/Components/Header.php";
+    if(isset($_GET["ruta"])){
+        if($_GET["ruta"] == "home"){
+            include_once "./src/View/HomeView.php";
+        }
+        else if($_GET["ruta"] == "movies"){
+            include_once "./src/Controller/AllProducts/MoviesController.php";
+        }
+         else if($_GET["ruta"] == "books"){
+            include_once "./src/Controller/AllProducts/BooksController.php";
+         }
+        else if($_GET["ruta"] == "music"){
+            include_once "./src/Controller/AllProducts/MusicController.php";
+        }
+        else if($_GET["ruta"] == "addMovie"){
+            include_once "./src/Controller/AddProduct/AddMovieController.php";
+        }
+        else if($_GET["ruta"] == "addBook"){
+            include_once "./src/Controller/AddProduct/AddBookController.php";
+        }
+        else if($_GET["ruta"] == "addDisc"){
+            include_once "./src/Controller/AddProduct/AddDiscController.php";
+        }
+        else if($_GET["ruta"] == "singleMovie" && isset($_GET["id"])){
+            include_once "./src/Controller/ShowProduct/ShowMovieController.php";
+        }
+        else if($_GET["ruta"] == "singleBook" && isset($_GET["id"])){
+            include_once "./src/Controller/ShowProduct/ShowBookController.php";
+        }
+        else if($_GET["ruta"] == "singleDisc" && isset($_GET["id"])){
+            include_once "./src/Controller/ShowProduct/ShowDiscController.php";
+         }
+        else if($_GET["ruta"] == "updateMovie" && isset($_GET["id"])){
+            include_once "./src/Controller/UpdateProduct/UpdateMovieController.php";
+        }
+        else if($_GET["ruta"] == "updateBook" && isset($_GET["id"])){
+            include_once "./src/Controller/UpdateProduct/UpdateBookController.php";
+        }
+        else if($_GET["ruta"] == "updateDisc" && isset($_GET["id"])){
+            include_once "./src/Controller/UpdateProduct/UpdateDiscController.php";
+         }
+    }
+    else{
 
-                }
-                include_once "./src/Components/Footer.php"; 
-            ?>
-        </main>
-        
-    </section>
-</body>
-</html>
+    }
+    include_once "./src/Components/Footer.php"; 
+?>
