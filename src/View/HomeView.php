35859a42943188de68978aa927eb4c254e1fedac
Mario Martín <mmartin.mrmg@gmail.com>
2024-01-16 10:55:18 +0100

    feat: ✨ Finish HomeView page

--- src/View/HomeView.php
+++ src/View/HomeView.php
@@ -1,4 +1,12 @@
 <div class="mainContainer">
     <link rel="stylesheet" href="<?php echo $ruta . "/public/css/ViewsCSS/home.css"; ?>">
     <h1 class="title">Bienvenido a VideoClub</h1>
+    <div class="cont">
+        <h2>¿Qué somos?</h2>
+        <p>Bienvenido a nuestro gestor de videoclub, la aplicación todo en uno que reúne la magia del cine, la fascinación de la música y la maravilla de la lectura en un solo lugar. Somos tu destino definitivo para explorar, organizar y disfrutar de una amplia colección de libros, películas y discos.</p>
+        <p>Puedes visitar nuestra página de documentación: <a href="https://marioby9.github.io/BookSphere-Doc" target="_blank" rel="noopener noreferrer" >VideoClub-Doc</a></p>
+        <h2>Sistema de desarrollo</h2>
+        <p>Para el desarrollo de VideoClub se ha seguido el sistema MVC (Modelo, Vista, Controlador), consiguiendo así una mayor facilidad para el desarrollo de la aplicación. </p>
+        <p>Para el sistema de bases de datos se ha utilizado la clase ORM, utilizado para mapear los objetos directamente a la base de datos. Se instancia un objeto de la clase indicada y se inserta o se obtienen sus datos mediante el ORM.</p>
+    </div>
 </div>
